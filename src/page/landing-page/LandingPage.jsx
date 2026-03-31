c4a150e5b1cc55da8e91a36d833d0e186e689eff
salmanxdev <salmanx.dev@gmail.com>
2026-03-31 13:24:26 +0530

    package-lock.json

---
+++ src/page/landing-page/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react' ;
+import { Link } from 'react-router-dom';
+
+function LandingPage() {
+    return (
+    <div className="landing-page">LANDING PAGE
+
+    </div>
+    )
+}
+export default LandingPage; 
