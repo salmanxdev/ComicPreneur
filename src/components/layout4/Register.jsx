ca989249e8c1c6c508389b5d6c6b4c563ff9491c
salmanxdev <salmanx.dev@gmail.com>
2026-04-13 10:21:17 +0530

    register.jsx updated

--- src/components/layout4/Register.jsx
+++ src/components/layout4/Register.jsx
@@ -261,8 +261,14 @@ const Register = ({ onClose }) => {
 
             <p className="qr-amount">
               Registration Charge:
-              <span style={{  marginLeft: "5px" }}>
+              <span style={{ textDecoration: "line-through", marginLeft: "5px" }}>
                 ₹99
+              </span>
+            </p>
+
+            <p>
+              <span style={{ color: "green", fontWeight: "bold", fontSize: "25px" }}>
+                Early Bird: ₹49/-
               </span>
             </p>
 
